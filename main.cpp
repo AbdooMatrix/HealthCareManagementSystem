5a5c1e7d3e0a5f01ec44cd5de4b28698e579ae13
Abdelrahman Mostafa <114619776+AbdooMatrix@users.noreply.github.com>
2024-11-22 14:18:09 +0200

    added the project to repo and added the main menu

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to Your Health Care Management System\n"
+            "Please enter a number that represents an option from the following:\n"
+            "1) Add New Doctor \n"
+            "2) Add New Appointment\n"
+            "3) Update Doctor Name (Doctor ID)\n"
+            "4) Update Appointment Date (Appointment ID)\n"
+            "5) Delete Appointment (Appointment ID)\n"
+            "6) Delete Doctor (Doctor ID)\n"
+            "7) Print Doctor Info (Doctor ID)\n"
+            "8) Print Appointment Info (Appointment ID)\n"
+            "9) Write Query\n"
+            "10) Exit\n"
+            "What's your choice? ";
+
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+
+            break;
+        case 9:
+
+            break;
+        case 10:
+
+            break;
+    }
+
+    return 0;
+}
